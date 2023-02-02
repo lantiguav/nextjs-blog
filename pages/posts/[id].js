807d82ee24d7cc5e1d455f483aba765ea407b7dd
Victor Lantigua <lantiguav@gmail.com>
2023-02-01 21:48:14 -0400

    fix: render post pody

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,13 +5,15 @@ import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
+
   return {
     props: {
       postData,
     },
   };
 }
+
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
